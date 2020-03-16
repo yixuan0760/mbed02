3f5f15547226a25c92abebdc73789918e8110137
xuan <fiona940731@gmail.com>
2020-03-16 12:43:39 -0400

    upload

---
+++ 2_2_Sawtooth_Waveform/main.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+
+
+AnalogOut Aout(D
+
+AC0_OUT);
+
+
+int main(){
+
+float i;
+
+  while(1){
+
+    for( i=0; i<1; i+=0.1 ){
+
+      Aout = i;
+
+      wait(0.001);
+
+    }
+
+  }
+
+}
